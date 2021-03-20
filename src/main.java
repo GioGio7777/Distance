e63b56d0940db92c7a63d76a1aba04ae76a1df50
Yusuf Çelik <58196464+GioGio7777@users.noreply.github.com>
2021-03-20 03:23:22 +0300

    begin

---
+++ src/main.java
@@ -0,0 +1,24 @@
+public class main {
+
+    double calculateMinkowski(double x[], double y[], double P1, double P2, double R) {
+        double result = 0;
+
+        for (int i = 0; i <x.length ; i++) { // For sigma in equation
+
+        }
+
+        return result;
+    }
+
+    double calculateMahalonobis(double x[], double y[], double P1, double P2) {
+        double result = 0;
+
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
