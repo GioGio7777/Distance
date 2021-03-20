b5a81d05e57651436ff6c856a31ab6b8abfa4eb9
Yusuf Çelik <58196464+GioGio7777@users.noreply.github.com>
2021-03-20 03:30:49 +0300

    minkowski finish

--- src/main.java
+++ src/main.java
@@ -2,17 +2,20 @@ public class main {
 
     double calculateMinkowski(double x[], double y[], double P1, double P2, double R) {
         double result = 0;
+        double plus = 0;
 
-        for (int i = 0; i <x.length ; i++) { // For sigma in equation
+        for (int i = 0; i < x.length; i++) { // For sigma in equation
+            plus += (x[0] - y[0]);
+
+            Math.pow(plus, R);
 
         }
 
-        return result;
+        return Math.pow(result, 1 / R);
     }
 
     double calculateMahalonobis(double x[], double y[], double P1, double P2) {
         double result = 0;
-
 
 
         return result;
